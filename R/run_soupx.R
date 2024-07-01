9b307b2f4031ec61b57b445b232bc68e354ad227
Griffen Wakelin <wakelin@dal.ca>
2024-07-01 17:01:15 -0300

    added documentation for `run_soupx`

--- R/run_soupx.R
+++ R/run_soupx.R
@@ -1,3 +1,20 @@
+#' Run SoupX decontamination
+#'
+#' `run_soupx()` wraps the normal SoupX workflow into a single function for convenience, with options
+#' as to whether the plots and outputs should be saved to disk.
+#'
+#' @param fpath Path to cellranger `outs` directory, which needs to contain `raw_feature_bc_matrix`, `filtered_feature_bc_matrix`, and `analysis` subdirectories.
+#' @param out_save Save SoupX output matrix?
+#' @param out_dir Path to output directory where results (and possibly plots) will be saved.
+#' @param plot_save Save SoupX plot?
+#' @param force If SoupX output matrix already found, should it be overwritten?
+#' @param verbose Be verbose?
+#'
+#' @return SoupX-corrected gene expression matrix.
+#' @export
+#'
+#' @examples
+#' soup <- wakelin::run_soupx(fpath = "scrnaseq/outs/tw2_uninjured/", out_save = TRUE, plot_save = TRUE)
 run_soupx <- function(fpath, out_save = FALSE, out_dir = "scrnaseq/outs_soupx/", plot_save = FALSE, force = FALSE, verbose = TRUE) {
   fname <- basename(tools::file_path_sans_ext(fpath))
   outfile <- paste0(out_dir, fname, "_soupx.mtx")
@@ -44,4 +61,5 @@ run_soupx <- function(fpath, out_save = FALSE, out_dir = "scrnaseq/outs_soupx/",
       Matrix::writeMM(soup_out, file = outfile)
     }
   }
+  soup_out
 }
