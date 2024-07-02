de9ec51f780c9119f70bd48d94dd33ac2239223a
Griffen Wakelin <wakelin@dal.ca>
2024-07-02 20:15:57 -0300

    Added `quick_mnn`.

--- R/quick_mnn.R
+++ R/quick_mnn.R
@@ -1,3 +1,14 @@
+#' MNN Integrate \linkS4class{SingleCellExperiment} Objects
+#'
+#' @param ... \linkS4class{SingleCellExperiment} classes to be MNN-integrated.
+#'
+#' @return \linkS4class{SingleCellExperiment} object with \code{corrected} object in \code{reducedDims}.
+#' @export
+#'
+#' @examples
+#' sce1 <- scater::mockSCE()
+#' sce2 <- scater::mockSCE()
+#' sce.combined <- quick_mnn(sce1, sce2)
 quick_mnn <- function(..., cluster = FALSE) {
   combined <- batchelor::correctExperiments(...)
   combined
