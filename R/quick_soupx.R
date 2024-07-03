31c7b6c7fb63d94bf34b838978050255fee0aaa9
Griffen Wakelin <wakelin@dal.ca>
2024-07-02 21:31:49 -0300

    Added custom loading function for 10x data since SoupX one was giving a headache.

--- R/quick_soupx.R
+++ R/quick_soupx.R
@@ -3,7 +3,7 @@
 #' Wraps the normal SoupX workflow, with default parameters, into a single
 #' function for convenience, with options as to whether the plots and outputs should be saved to disk.
 #'
-#' @param fpath A character vector indicating cellranger \code{outs} directory, which itself which must contain \code{raw_feature_bc_matrix}, \code{filtered_feature_bc_matrix}, and \code{analysis} subdirectories.
+#' @param fpath A character vector indicating cellranger \code{outs} directory, which itself which must contain \code{raw_feature_bc_matrix} and \code{filtered_feature_bc_matrix}} subdirectories.
 #' @param out_save Logical scalar indicating whether SoupX-corrected matrix should be written to disk.
 #' @param out_dir A character vector indicating the directory to save outputs in.
 #' @param plot_save Logical scalar indicating whether SoupX plot should be saved to disk.
@@ -18,7 +18,7 @@
 #' soup <- quick_soupx(fpath = fpath, out_save = FALSE, plot_save = FALSE)
 #'
 #' @importFrom Matrix writeMM
-#' @importFrom SoupX load10X autoEstCont adjustCounts
+#' @importFrom SoupX autoEstCont adjustCounts
 #' @importFrom grDevices svg dev.off
 quick_soupx <- function(fpath, out_save = FALSE, out_dir = "scrnaseq/outs_soupx/", plot_save = FALSE, force = FALSE, verbose = TRUE) {
   fname <- get_fname(fpath)
@@ -34,7 +34,7 @@ quick_soupx <- function(fpath, out_save = FALSE, out_dir = "scrnaseq/outs_soupx/
   }
 
   msg_verbose("Opening dataset '", fname, "'...")
-  soup <- load10X(fpath, verbose = verbose)
+  soup <- .load10x(fpath)
   if (plot_save) {
     if (!dir.exists(out_dir)) {
       dir.create(out_dir)
@@ -57,4 +57,22 @@ quick_soupx <- function(fpath, out_save = FALSE, out_dir = "scrnaseq/outs_soupx/
     writeMM(soup_out, file = outfile)
   }
   soup_out
+}
+
+.load10x <- function(fpath) {
+  # designed and tested for 10x 3' gene expression data from cellranger-8.0.1
+  fpath_raw <- file.path(fpath, "raw_feature_bc_matrix")
+  fpath_cells <- file.path(fpath, "filtered_feature_bc_matrix")
+  dat_raw <- DropletUtils::read10xCounts(fpath_raw, row.names = "symbol", col.names = TRUE)
+  dat_cells <- DropletUtils::read10xCounts(fpath_cells, row.names = "symbol", col.names = TRUE)
+  dat_cells <- scater::logNormCounts(dat_cells)
+  dat_cells <- scater::runPCA(dat_cells)
+  snn <- scran::buildSNNGraph(dat_cells, use.dimred = "PCA")
+  clusters <- igraph::cluster_walktrap(snn)$membership
+  soup_channel <- SoupX::SoupChannel(
+    tod = SingleCellExperiment::counts(dat_raw),
+    toc = SingleCellExperiment::counts(dat_cells),
+    metaData = data.frame(clusters=clusters,row.names = colnames(dat_cells)),
+  )
+  soup_channel
 }
